3961702c4936a9aa83d63937fc4825aed3c15a84
kimgoat <kimgoatlove@gmail.com>
2024-01-15 21:18:11 +0900

    배경 이미지 설정 기능 추가

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -2,11 +2,26 @@ import styles from "./MainBoard.module.css";
 import { Resizable } from "re-resizable";
 import Header from "./Header";
 import List from "./List";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DarkModeProvider } from "../context/DarkModeContext";
 
 export default function MainBoard() {
   const [itemFilter, setItemFilter] = useState("All");
+  const [backgroundImg, setBackgroundImg] = useState(null);
+
+  useEffect(() => {
+    const localData = localStorage.getItem("background-img");
+    if (localData) {
+      setBackgroundImg(JSON.parse(localData));
+    }
+  }, []);
+
+  const handleChangeImg = (e) => {
+    const file = e.target.files[0];
+    const imgUrl = URL.createObjectURL(file);
+    setBackgroundImg(imgUrl);
+    localStorage.setItem("background-img", JSON.stringify(imgUrl));
+  };
 
   const handleFilter = (state) => {
     setItemFilter(state);
@@ -14,9 +29,21 @@ export default function MainBoard() {
 
   return (
     <DarkModeProvider>
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        style={{
+          backgroundImage: `url(${backgroundImg})`,
+        }}
+      >
+        <input
+          type="file"
+          accept="image/*"
+          required
+          multiple
+          onChange={handleChangeImg}
+        />
         <Resizable
-          defaultSize={{ width: 500, height: 600 }}
+          defaultSize={{ width: 700, height: 600 }}
           minHeight={600}
           maxHeight={600}
           minWidth={400}
