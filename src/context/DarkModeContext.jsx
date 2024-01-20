90144840caf9610538eb6af296f697b928e78eee
kimgoat <kimgoatlove@gmail.com>
2024-01-20 18:23:22 +0900

    update darkmode: localstorage, ...

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,13 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const DarkModeContext = createContext();
 
 export function DarkModeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => setDarkMode((mode) => !mode);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    updateDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefer-color-scheme: dark)").matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
+
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   );
+}
+
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  }
 }
